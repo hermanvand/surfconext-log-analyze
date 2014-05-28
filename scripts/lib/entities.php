11455666314ee614c6062a0ec26ea0a05bf483fb
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2014-05-28 13:32:55 +0200

    Fixes for new Janus databse format

--- scripts/lib/entities.php
+++ scripts/lib/entities.php
@@ -52,11 +52,11 @@ function getAllEntities() {
 	$idp_index = array();
 	
 	# order by is crucial for building data structure
-	$result = mysql_query("SELECT eid,entityid,revisionid,state,type,created FROM ".$LA['table_entities']." ORDER BY eid,revisionid ASC", $LA['mysql_link_sr']);
+	$result = mysql_query("SELECT id,eid,entityid,revisionid,state,type,created FROM ".$LA['table_entities']." ORDER BY eid,revisionid ASC", $LA['mysql_link_sr']);
 	
 	if ($result) {
 		while ($result_row = mysql_fetch_assoc($result)) {
-			$id=0;
+			$eid=0;
 			$name="";
 			$revision=0;
 			$timestamp=NULL;
@@ -64,7 +64,8 @@ function getAllEntities() {
 			$provider="";
 
 			# reformat
-			$id = $result_row['eid'];
+			$conrev_id = $result_row['id'];
+			$eid = $result_row['eid'];
 			$name = $result_row['entityid'];
 			$revision = $result_row['revisionid'];
 
@@ -86,8 +87,8 @@ function getAllEntities() {
 			}
 
 			# first entry
-			if (! array_key_exists($id, $entities)) {
-				$entities[$id] = array();
+			if (! array_key_exists($eid, $entities)) {
+				$entities[$eid] = array();
 				$prev_env = "";
 				$prev_name = "";
 				$prev_extra = "";
@@ -103,11 +104,12 @@ function getAllEntities() {
 				if ( strncasecmp($metadata['type'], $provider, 1) != 0 ) continue;
 
 				# fetch metadata
-				$result2 = mysql_query("
+				$q = "
 					SELECT `value` FROM {$LA['table_metadata']}
-					WHERE (`eid`,`revisionid`) = ({$id},{$revision})
+					WHERE `connectionRevisionId` = {$conrev_id}
 					AND `key`='{$key}'
-				");
+				";
+				$result2 = mysql_query($q,$LA['mysql_link_sr']);
 				if ($result2 && mysql_num_rows($result2)==1) {
 					$result_row = mysql_fetch_array($result2);
 					$value = $result_row[0];
@@ -117,10 +119,10 @@ function getAllEntities() {
 
 			# Be smart, only consider revision with changes in environment :-)
 			if ( $environment != $prev_env || !array_equal($extra,$prev_extra) ) {
-				$entities[$id][$revision] = array();
-				$entities[$id][$revision]['timestamp'] = $timestamp;
-				$entities[$id][$revision]['environment'] = $environment;
-				$entities[$id][$revision]['metadata'] = $extra;
+				$entities[$eid][$revision] = array();
+				$entities[$eid][$revision]['timestamp'] = $timestamp;
+				$entities[$eid][$revision]['environment'] = $environment;
+				$entities[$eid][$revision]['metadata'] = $extra;
 
 				$prev_extra = $extra;
 				$prev_env   = $environment;
@@ -130,10 +132,10 @@ function getAllEntities() {
 			if ($name != $prev_name) {		
 				if ( $name != $prev_name ) {
 					if ($provider == "S") {
-						$sp_index[$name] = $id;
+						$sp_index[$name] = $eid;
 					}
 					else {
-						$idp_index[$name] = $id;
+						$idp_index[$name] = $eid;
 					}
 				}
 				$prev_name = $name;
